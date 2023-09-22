84e6d41c08a1490d3b1515eacae90134b7d83f50
jmarqu27 <81587383+jmarqu27@users.noreply.github.com>
2023-09-21 20:35:32 -0400

    original version

---
+++ messageGenerator.js
@@ -0,0 +1,47 @@
+
+let joke0 = "I'm afraid for the calendar. Its days are numbered.";
+let joke1 = "What do you call a fish wearing a bowtie? Sofishticated.";
+let joke2 = "Dear Math, grow up and solve your own problems.";
+let joke3 = "What did the ocean say to the beach? Nothing, it just waved.";
+let joke4 = "I only know 25 letters of the alphabet. I don't know y.";
+let joke5 = "How does the moon cut his hair? Eclipse it.";
+let joke6 = "Where do fruits go on vacation? Pear-is!";
+let joke7 = "Dad, did you get a haircut? No, I got them all cut!";
+let joke8 = "What do you call a poor Santa Claus? St. Nickel-less.";
+let joke9 = "How do you get a squirrel to like you? Act like a nut.";
+
+let randomJoke = Math.floor(Math.random() * 9);
+
+switch(randomJoke) {
+    case 0:
+        console.log(joke0);
+        break;
+    case 1:
+        console.log(joke1);
+        break;
+    case 2:
+        console.log(joke2);
+        break;
+    case 3:
+        console.log(joke3);
+        break;
+    case 4:
+        console.log(joke4);
+        break;
+    case 5:
+        console.log(joke5);
+        break;
+    case 6:
+        console.log(joke6);
+        break;
+    case 7:
+        console.log(joke7);
+        break;
+    case 8:
+        console.log(joke8);
+        break;
+    case 9:
+        console.log(joke9);
+        break;
+}
+
